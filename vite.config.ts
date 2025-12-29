14ffb143eff07ba8da88fe8c0ad2819cdfbb55e1
narcisomanjate-jpg <narciso.manjate@gmail.com>
2025-12-29 12:21:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/Super-Agente-MNG-NICE/', // Linha adicionada para o GitHub Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
